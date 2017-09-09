71cb4545bbdbc7e41502c63e39ad7b21ef53b570
theodore3131 <alantheodore32@gmail.com>
2017-09-09 21:10:01 +0800

    first edition

--- TipCalculator/ViewController.swift
+++ TipCalculator/ViewController.swift
@@ -10,6 +10,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var txtBill: UITextField!
+    
+    @IBOutlet weak var txtTip: UITextField!
+    
+    @IBOutlet weak var lblTipAmount: UILabel!
+    
+    @IBAction func CalculateTip(_ sender: Any) {
+        let billAmount = (Double)(txtBill.text!)
+        let pctTip = (Double)(txtTip.text!)
+        let tipAmount = billAmount! * pctTip!/100
+        let tipString = String(format:"%.2f",tipAmount)
+        lblTipAmount.text = "$" + tipString
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
